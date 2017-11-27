281b13c6278d048954dc4d45bb631575a6e4b8fb
jyparketc <jy-.park@samsung.com>
2017-11-27 14:26:08 +0900

    Create server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import tensorflow as tf
+import time
+
+t_end = time.time() + 60 * 5
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+while time.time() < t_end:
+  print(sess.run(hello))
