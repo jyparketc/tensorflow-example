6354386115903e59e5fccba8e88a7164dbed04d2
jyparketc <jy-.park@samsung.com>
2018-01-15 11:16:37 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,8 @@ from __future__ import print_function
 import tensorflow as tf
 import time
 
-t_end = time.time() + 60 * 5
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
-while time.time() < t_end:
+while True:
   time.sleep(10)
   print(sess.run(hello))
